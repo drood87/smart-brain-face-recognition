9d25cd92e881bd3e033835d6100d4c8aa5c15a22
drood <dbernstein1987@hotmail.com>
2019-03-11 18:03:12 +1100

    adding routing and bounding box for face rec

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import SignIn from './components/SignIn/SignIn';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
 
@@ -13,7 +14,7 @@ import './App.css';
 // console.log(`${process.env.REACT_APP_CLARIFAI_API_KEY}`);
 
 const app = new Clarifai.App({
-  apiKey: "you don't get my key"
+  apiKey: 'nope'
 });
 
 const particlesOptions = {
@@ -29,13 +30,41 @@ const particlesOptions = {
 };
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      input: '',
-      imageUrl: ''
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     input: '',
+  //     imageUrl: ''
+  //   };
+  // }
+
+  state = {
+    input: '',
+    imageUrl: '',
+    box: {},
+    route: 'signin'
+  };
+
+  calculateFaceLocation = data => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box; //gets the data provided by the API at what % the box for the face is
+
+    const image = document.getElementById('inputImage'); //grabs the image element from the DOM
+    const width = Number(image.width); // gets the image width
+    const height = Number(image.height); //gets the image height
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
     };
-  }
+  };
+
+  displayFaceBox = box => {
+    console.log(box);
+    this.setState({ box: box }); // saves the calculated facebox return value and sets it as state in the box object
+  };
 
   onInputChange = event => {
     this.setState({ input: event.target.value });
@@ -43,16 +72,13 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      function(response) {
-        console.log(
-          response.outputs[0].data.regions[0].region_info.bounding_box
-        );
-      },
-      function(err) {
-        // there was an error
-      }
-    );
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response =>
+        this.displayFaceBox(this.calculateFaceLocation(response)).catch(err =>
+          console.log(err)
+        )
+      );
   };
 
   render() {
@@ -60,13 +86,22 @@ class App extends Component {
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
         <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit}
-        />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        {this.state.route === 'signin' ? (
+          <SignIn />
+        ) : (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition
+              box={this.state.box}
+              imageUrl={this.state.imageUrl}
+            />
+          </div>
+        )}
       </div>
     );
   }
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className="f3">
-        {'This Magic brain will detect faces in your picture. Give it a go'}
+        {'This Magic Brain will detect faces in your picture. Give it a go'}
       </p>
       <div className="center">
         <div className="form center pa4 br3 shadow-5">
